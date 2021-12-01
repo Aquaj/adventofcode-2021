266fe683bc7843b9a227eebab1d7a52d0bc97b7d
Jérémie <jeremie.bonal@gmail.com>
2021-12-01 06:11:38 +0100

    Day 01

--- day-01.rb
+++ day-01.rb
@@ -2,15 +2,17 @@ require_relative 'common'
 
 class Day1 < AdventDay
   def first_part
+    input.each_cons(2).count { |a,b| b > a }
   end
 
   def second_part
+    input.each_cons(3).each_cons(2).count { |a,b| b.sum > a.sum }
   end
 
   private
 
   def convert_data(data)
-    super
+    super.map(&:to_i)
   end
 end
 
