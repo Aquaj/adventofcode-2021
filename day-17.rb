6ed2fb024d6d2e938d522282c448033b5ad52fba
Jérémie <jeremie.bonal@gmail.com>
2021-12-18 11:08:54 +0100

    Day 17+18

--- day-17.rb
+++ day-17.rb
@@ -1,16 +1,68 @@
 require_relative 'common'
 
 class Day17 < AdventDay
+  EXPECTED_RESULTS = { 1 => 45, 2 => 112 }.freeze
+
   def first_part
+    trajectories.
+      select { |(vx,vy),pos| pos.any? { |coords| in_target?(*coords) } }.
+      map { |*_,pos| pos.map(&:last).max }.
+      max
   end
 
   def second_part
+    trajectories.
+      select { |(vx,vy),pos| pos.any? { |coords| in_target?(*coords) } }.
+      count
   end
 
   private
 
+  def in_target?(x,y)
+    xmin,xmax,ymax,ymin = input
+    xmin.towards(xmax).include?(x) && ymin.towards(ymax).include?(y)
+  end
+
+  def after_target?(x,y)
+    outer_x,outer_y = outer_coords
+
+    sign_y = outer_y / outer_y.abs
+    x.abs > outer_x.abs || ((y-outer_y)*sign_y).positive?
+  end
+
+  def outer_coords
+    return @outer if defined? @outer
+    xmin,xmax,ymax,ymin = input
+    outer_x = [xmin,xmax].max_by(&:abs)
+    outer_y = [ymin,ymax].max_by(&:abs)
+    @outer = [outer_x, outer_y]
+  end
+
+  def trajectories
+    xmin,xmax,ymax,ymin = input
+    outer_x,outer_y = outer_coords
+
+    y_range = outer_y.positive? ? 0.towards(2*outer_y) : (-1*outer_y).towards(outer_y)
+    vels = y_range.flat_map do |vel_y|
+      0.towards(outer_x).map do |vel_x|
+        positions = []
+        new_x = 0
+        new_y = 0
+        (0...).each do |n|
+          break if after_target?(new_x, new_y)
+          sign_x = vel_x.zero? ? 0 : vel_x / vel_x.abs
+          abs_pos_x = [vel_x.abs * (n+1) - (0..[n,vel_x].min).sum, (0..vel_x.abs).sum].min
+          new_x = sign_x * abs_pos_x
+          new_y = vel_y*(n+1) - (0..n).sum
+          positions << [new_x, new_y]
+        end
+        [[vel_x, vel_y], positions]
+      end
+    end
+  end
+
   def convert_data(data)
-    super
+    super.first.match(/target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/).captures.map(&:to_i)
   end
 end
 
--- day-18.rb
+++ day-18.rb
@@ -1,16 +1,124 @@
 require_relative 'common'
 
 class Day18 < AdventDay
+  EXPECTED_RESULTS = { 1 => 4140, 2 => 3993 }.freeze
+
   def first_part
+    input.reduce(&:+).magnitude
   end
 
   def second_part
+    input.combination(2).flat_map do |(a,b)|
+      forward = a.copy + b.copy
+      backward = b.copy + a.copy
+      [forward.magnitude, backward.magnitude]
+    end.max
   end
 
   private
 
+  require 'json'
   def convert_data(data)
-    super
+    super.map do |number|
+      number = JSON(number)
+      Node.new(number)
+    end
+  end
+
+  class Node
+    def initialize(value)
+      @type = value.is_a?(Integer) ? :value : :pair
+      if value?
+        @value = value
+      else
+        @value = value.map { |n| n.is_a?(Node) ? n : Node.new(n) }
+      end
+    end
+
+    def replace(value)
+      @type = value.is_a?(Integer) ? :value : :pair
+      @value = value
+    end
+
+    attr_reader :value
+
+    def value?; @type == :value; end
+    def pair?; @type == :pair; end
+    def inspect; @value.inspect; end
+
+    def magnitude
+      return @value if value?
+      return @value.first.magnitude * 3 + value.last.magnitude * 2
+    end
+
+    def +(node)
+      self.class.new([self, node]).reduce
+    end
+
+    def reduce
+      loop do
+        case
+        when pair = leftmost_deep_nested_pair
+          pair.explode(onto: neighbors_of(pair))
+        when node = leftmost_high_node
+          node.split
+        else
+          break self
+        end
+      end
+    end
+
+    def leftmost_deep_nested_pair(nesting: 0)
+      return self if nesting == 4 && pair?
+      @value.find do |part|
+        next if part.value?
+        pair = part.leftmost_deep_nested_pair(nesting: nesting + 1)
+        break pair if pair
+      end
+    end
+
+    def leftmost_high_node
+      return self if value? && value >= 10
+      return if value?
+      @value.find do |part|
+        too_high = part.leftmost_high_node
+        break too_high if too_high
+      end
+    end
+
+    def explode(onto:)
+      elements = self.flatten.map(&:value)
+
+      left, right = *onto
+      left.replace(left.value + elements.first) if left
+      right.replace(right.value + elements.last) if right
+
+      self.replace(0)
+    end
+
+    def split
+      self.replace [Node.new(value / 2), Node.new((value/2.0).ceil)]
+    end
+
+    def neighbors_of(element)
+      elements = element.flatten
+
+      tuple_and_neighbors = self.flatten.each_cons(elements.length+1)
+      left = tuple_and_neighbors.find  { |n, *p| p == elements }&.first
+      right = tuple_and_neighbors.find { |*p,n| p == elements }&.last
+
+      [left, right]
+    end
+
+    def flatten
+      return self if value?
+      value.flat_map(&:flatten)
+    end
+
+    def copy
+      return Node.new(self.value) if value?
+      Node.new(@value.map { |part| part.copy })
+    end
   end
 end
 
