5dd005946309d9c6e56f19eb8457419bda467e13
Jérémie <jeremie.bonal@gmail.com>
2021-12-22 13:25:14 +0100

    Day 21 performance ameliorations.

--- day-21.rb
+++ day-21.rb
@@ -3,6 +3,8 @@ require_relative 'common'
 class Day21 < AdventDay
   EXPECTED_RESULTS = { 1 => 739785, 2 => 444356092776315 }.freeze
 
+  UNIVERSE_FREQUENCIES = (1..3).product(1..3).product(1..3).map(&:flatten).tally
+
   def first_part
     dice = (1..100).cycle.each_slice(3)
 
@@ -23,30 +25,34 @@ class Day21 < AdventDay
   end
 
   def second_part
-    @cache = {}
-    game(input[0], input[1], 0, 0, 0, 0).max
+    @cache = Hash.new
+    GC.disable # Slight perf boost sicne we're going to create a bunch of objects
+    scores = cached_game(input[0], input[1], 0, 0, 0, 0)
+    GC.enable
+    scores.max
   end
 
   private
 
-  def game(pos_1, pos_2, score_1, score_2, dice_value, turn, wins_1=0, wins_2=0)
-    universe_wins_1, universe_wins_2 = *@cache[[pos_1, pos_2, score_1, score_2, dice_value, turn]] ||= begin
-      score_1 += (pos_1 = (pos_1 + dice_value - 1) % 10 + 1) if turn == 1
-      return [wins_1+1, wins_2] if score_1 >= 21
-      score_2 += (pos_2 = (pos_2 + dice_value - 1) % 10 + 1) if turn == 2
-      return [wins_1, wins_2+1] if score_2 >= 21
-      new_turn = turn % 2 + 1
+  def game(pos_1, pos_2, score_1, score_2, dice_value, turn)
+    score_1 += (pos_1 = (pos_1 + dice_value - 1) % 10 + 1) if turn == 1
+    return [1, 0] if score_1 >= 1000
+    score_2 += (pos_2 = (pos_2 + dice_value - 1) % 10 + 1) if turn == 2
+    return [0, 1] if score_2 >= 1000
+    new_turn = turn % 2 + 1
 
-      universe_wins_1 = universe_wins_2 = 0
-      (1..3).product(1..3).product(1..3).each do |die_roll|
-        w1, w2 = *game(pos_1, pos_2, score_1, score_2, die_roll.flatten.sum, new_turn, 0, 0)
-        universe_wins_1 += w1
-        universe_wins_2 += w2
-      end
-      [universe_wins_1, universe_wins_2]
+    universe_wins_1 = universe_wins_2 = 0
+    UNIVERSE_FREQUENCIES.each do |(a,b,c), frequency|
+      w1, w2 = *cached_game(pos_1, pos_2, score_1, score_2, a+b+c, new_turn)
+      universe_wins_1 += w1 * frequency
+      universe_wins_2 += w2 * frequency
     end
+    [universe_wins_1, universe_wins_2]
+  end
 
-    [wins_1 + universe_wins_1, wins_2 + universe_wins_2]
+  def cached_game(*game_configuration)
+    cache_key = game_configuration.hash # Pre-hashing so Array#eql? isn't called on cache lookup
+    @cache[cache_key] ||= game(*game_configuration)
   end
 
   def convert_data(data)
